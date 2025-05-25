568c581f18403e4c5d70085a592d4af9ab658bfb
Diego Novais <die344g@gmail.com>
2025-05-25 10:12:15 -0400

    Payment, one-to-one association

--- src/main/java/praticando/spring/curso/config/TestConfig.java
+++ src/main/java/praticando/spring/curso/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import praticando.spring.curso.entities.Category;
 import praticando.spring.curso.entities.Order;
 import praticando.spring.curso.entities.OrderItem;
+import praticando.spring.curso.entities.Payment;
 import praticando.spring.curso.entities.Product;
 import praticando.spring.curso.entities.User;
 import praticando.spring.curso.entities.enums.OrderStatus;
@@ -80,6 +81,11 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T19:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 	}
 	
 }
